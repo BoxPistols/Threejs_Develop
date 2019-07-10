b9374d5a25783654247565718f6a38005b68ea20
ait <ait@ex.com>
2019-07-11 08:16:42 +0900

    add helper

--- app.js
+++ app.js
@@ -1,43 +1,59 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    var scene;
-    var camera;
-    var box;
-    var light;
-    var ambient;
-    var renderer;
-    var width = 500;
-    var height = 250;
+  var scene;
+  var camera;
+  var box;
+  var light;
+  var ambient;
+  var renderer;
+  var width = 500;
+  var height = 400;
 
-    scene = new THREE.Scene()
+  var gridHelper;
+  var axisHelper;
+  var lightHelper;
 
-    box = new THREE.Mesh(
-        new THREE.BoxGeometry(50, 50, 50),
-        new THREE.MeshLambertMaterial({ color: 0xff0000 })
-    );
-    box.position.set(0, 0, 0);
-    scene.add(box);
+  scene = new THREE.Scene()
 
-    light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(0, 60, 30);
-    scene.add(light);
+  box = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 50, 50),
+    new THREE.MeshLambertMaterial({
+      color: 0xff0000
+    })
+  );
+  box.position.set(0, 0, 0);
+  scene.add(box);
 
-    ambient = new THREE.AmbientLight(0x040404);
-    light.position.set(0, 60, 30);
-    scene.add(ambient);
+  light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(0, 60, 30);
+  scene.add(light);
 
+  ambient = new THREE.AmbientLight(0x040404);
+  light.position.set(0, 60, 30);
+  scene.add(ambient);
 
-    camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
-    camera.position.set(200, 100, 300);
-    camera.lookAt(scene.position);
+  camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+  camera.position.set(200, 80, 200);
+  camera.lookAt(scene.position);
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(width, height);
-    renderer.setClearColor(0xefefef);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    document.getElementById('stage').appendChild(renderer.domElement);
-    renderer.render(scene, camera);
+  gridHelper = new THREE.GridHelper(1000, 50);
+  scene.add(gridHelper);
+
+  axisHelper = new THREE.AxisHelper(1000);
+  scene.add(axisHelper);
+
+  lightHelper = new THREE.DirectionalLightHelper(light, 10);
+  scene.add(lightHelper);
+
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+  renderer.setSize(width, height);
+  renderer.setClearColor(0xefefef);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  document.getElementById('stage').appendChild(renderer.domElement);
+  renderer.render(scene, camera);
 
 
 })();
